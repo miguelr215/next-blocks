230e05ad6f344c879bdb2433f369d898a8d09caf
miguelr215 <drexelramos@gmail.com>
2026-02-16 14:37:33 -0500

    logout implemented

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,8 +1,12 @@
+import Logout from '@/components/logout'
 import React from 'react'
 
 const Dashboard = () => {
     return (
-        <h1>Dashboard</h1>
+        <main>
+            <h1>Dashboard</h1>
+            <Logout />
+        </main>
     )
 }
 
