1cb3aefe616cc3b61a63a670fd919a2781c28582
miguelr215 <drexelramos@gmail.com>
2026-02-16 13:44:03 -0500

    google login implemented

--- lib/auth.ts
+++ lib/auth.ts
@@ -5,6 +5,13 @@ import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { nextCookies } from "better-auth/next-js";
 
 export const auth = betterAuth({
+	baseURL: process.env.BETTER_AUTH_URL,
+	socialProviders: {
+		google: {
+			clientId: process.env.GOOGLE_CLIENT_ID as string,
+			clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+		},
+	},
 	emailAndPassword: {
 		enabled: true,
 	},
