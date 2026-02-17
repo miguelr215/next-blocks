f3e1e0a78e2055e1889954e69e3ca37893df1e3b
miguelr215 <drexelramos@gmail.com>
2026-02-17 05:55:37 -0500

    logged in redirect

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,8 +2,19 @@ import SmallLogo from "@/components/ui/smallLogo"
 
 import { LoginForm } from "@/components/login-form"
 import Link from "next/link"
+import { auth } from "@/lib/auth"
+import { headers } from "next/headers"
+import { redirect } from "next/navigation"
 
-export default function LoginPage() {
+export default async function LoginPage() {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  })
+
+  if (session) {
+    redirect("/dashboard")
+  }
+
   return (
     <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
       <div className="flex w-full max-w-sm flex-col gap-6">
