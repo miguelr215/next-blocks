0924c959219f7f3eb36d9dcfb3127739e9bfcf26
miguelr215 <drexelramos@gmail.com>
2026-02-22 16:08:58 -0500

    sports cards

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <div className="">
-      <h1>home page</h1>
+      <h1 className="page-title">home page</h1>
 
     </div>
   );
--- app/sports/page.tsx
+++ app/sports/page.tsx
@@ -1,22 +1,39 @@
 import Link from 'next/link'
 import React from 'react'
+import SpotlightCard from '@/components/SpotlightCard';
 
 const SportsPage = () => {
     return (
         <div>
-            <h1>Sports</h1>
-            <ul>
+            <h1 className="page-title">Sports</h1>
+            <ul className='flex flex-col gap-4 sm:grid sm:grid-cols-2 sm:gap-6 lg:grid-cols-4'>
                 <li>
-                    <Link href="/nfl">NFL</Link>
+                    <Link href="/nfl">
+                        <SpotlightCard className="custom-spotlight-card" spotlightColor="rgba(0, 229, 255, 0.2)">
+                            <h2>NFL</h2>
+                        </SpotlightCard>
+                    </Link>
                 </li>
                 <li>
-                    <Link href="/nba">NBA</Link>
+                    <Link href="/nba">
+                        <SpotlightCard className="custom-spotlight-card" spotlightColor="rgba(225, 0, 225, 0.2)">
+                            <h2>NBA</h2>
+                        </SpotlightCard>
+                    </Link>
                 </li>
                 <li>
-                    <Link href="/mlb">MLB</Link>
+                    <Link href="/mlb">
+                        <SpotlightCard className="custom-spotlight-card" spotlightColor="rgba(25, 255, 52, 0.2)">
+                            <h2>MLB</h2>
+                        </SpotlightCard>
+                    </Link>
                 </li>
                 <li>
-                    <Link href="/nhl">NHL</Link>
+                    <Link href="/nhl">
+                        <SpotlightCard className="custom-spotlight-card" spotlightColor="rgba(25, 25, 255, 0.2)">
+                            <h2>NHL</h2>
+                        </SpotlightCard>
+                    </Link>
                 </li>
             </ul>
         </div>
