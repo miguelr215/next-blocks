59eb6a390b8695b8ee532f0bc7749913f92528ed
miguelr215 <drexelramos@gmail.com>
2026-03-05 20:44:21 -0500

    how to play page

--- app/how-to-play/page.tsx
+++ app/how-to-play/page.tsx
@@ -5,11 +5,11 @@ import React from 'react'
 
 const HowToPlayPage = () => {
     return (
-        <div>
+        <>
             <HowToPlayHero />
             <HowToPlaySection />
             <HowToWinSection />
-        </div>
+        </>
     )
 }
 
